7c3e57532b80f6c733eb0f0fbc13eed528b43b53
Xige Michael Chen <mxchen2001@gmail.com>
2020-09-23 15:11:48 -0500

    added starter files, documentation and dependencies

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Project 3 Word Ladder
 ### Xige Michael Chen, xmc75
 ### Jeong Woo Park, (update with EID)
+
+The `five_letter_words.txt` file needs to be outside of the src folder while being tested. This is because the starter code for the assignment includes a `makeDictionary` method with this line in it: 
+
+```
+infile = new Scanner (new File("five_letter_words.txt"));
+```
+
+
+For example, you have a project named as assignment3. The `five_letter_words.txt` should be under assignment3. Not under src, not under other packages. It should just under assignment3.
---
+++ assignment3/Main.java
@@ -0,0 +1,125 @@
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Git URL:
+ * Fall 2020
+ */
+
+
+package assignment3;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	// static variables and constants only here.
+	private static final boolean DEBUG = true;
+	private static LinkedList<String> adj[]; //Adjacency Lists 
+	private static Set<String> wordSet;
+	private static ArrayList<String> input;
+	
+	public static void main(String[] args) throws Exception {
+		
+		Scanner kb;	// input Scanner for commands
+		PrintStream ps;	// output file, for student testing and grading only
+		// If arguments are specified, read/write from/to files instead of Std IO.
+		if (args.length != 0) {
+			kb = new Scanner(new File(args[0]));
+			ps = new PrintStream(new File(args[1]));
+			System.setOut(ps);			// redirect output to ps
+		} else {
+			kb = new Scanner(System.in);// default input from Stdin
+			ps = System.out;			// default output to Stdout
+		}
+		initialize();
+
+		
+		// TODO methods to read in words, output ladder
+	}
+	
+	public static void initialize() {
+		// initialize your static variables or constants here.
+		// We will call this method before running our JUNIT tests.  So call it 
+		// only once at the start of main.
+		wordSet = makeDictionary();
+		System.out.println(wordSet);
+	}
+	
+	/**
+	 * @param keyboard Scanner connected to System.in
+	 * @return ArrayList of Strings containing start word and end word. 
+	 * If command is /quit, return empty ArrayList. 
+	 */
+	public static ArrayList<String> parse(Scanner keyboard) {
+		// TO DO
+		String start = keyboard.nextLine();
+		String end = keyboard.nextLine();
+
+		// Condition that the either input is "/quit"
+		if (start.equals("/quit") || end.equals("/quit")) {
+			return new ArrayList<String>();
+		}
+		ArrayList<String> input = new ArrayList<String>();
+		input.add(start);
+		input.add(end);
+
+		return input;
+	}
+	
+	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		
+		// Returned list should be ordered start to end.  Include start and end.
+		// If ladder is empty, return list with just start and end.
+		// TODO some code
+		
+
+
+		return null; // replace this line later with real return
+	}
+	
+    public static ArrayList<String> getWordLadderBFS(String start, String end) {
+		
+		// TODO some code
+		
+		return null; // replace this line later with real return
+	}
+    
+	
+	public static void printLadder(ArrayList<String> ladder) {
+		for (int i = 0; i < ladder.size(); i++) {
+			System.out.println(ladder.get(i));
+		}
+	}
+	// TODO
+	// Other private static methods here
+
+
+	/* Do not modify makeDictionary */
+	public static Set<String>  makeDictionary () {
+		Set<String> words = new HashSet<String>();
+		Scanner infile = null;
+		try {
+			/* *** Modified for DEBUGGING purposes *** 
+			 * *** MAKE SURE TO REVERT CHANGES ***
+			 */
+			String fileName = "five_letter_words.txt";
+			if (DEBUG) fileName = "short_dict.txt";
+			infile = new Scanner (new File(fileName));
+		} catch (FileNotFoundException e) {
+			System.out.println("Dictionary File not Found!");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while (infile.hasNext()) {
+			words.add(infile.next().toUpperCase());
+		}
+		return words;
+	}
+}
